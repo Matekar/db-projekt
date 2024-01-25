38028d5a6952c99df1ad20afda119c19eb90a46c
Matekar <kiewskimarcin@gmail.com>
2024-01-25 17:23:41 +0100

    hide detail if empty

--- src/routes/tabela/artykuly/[id]/+page.svelte
+++ src/routes/tabela/artykuly/[id]/+page.svelte
@@ -7,32 +7,34 @@
 <section>
 	<table>
 		{#each Object.keys(data.props.data) as key}
-			<tr>
-				<td>{key}</td>
-				<td>
-					{#if key == 'autorzy'}
-						<ul>
-							{#each data.props.data[key] as autor}
-								<li>{autor.imie} {autor.nazwisko}</li>
-							{/each}
-						</ul>
-					{:else if key == 'redaktorzy'}
-						<ul>
-							{#each data.props.data[key] as redaktor}
-								<li>{redaktor.imie_redaktor} {redaktor.nazwisko_redaktor}: {redaktor.rola}</li>
-							{/each}
-						</ul>
-					{:else if key == 'cytuje'}
-						<ul>
-							{#each data.props.data[key] as cyt}
-								<li>{cyt.tytul}</li>
-							{/each}
-						</ul>
-					{:else}
-						{data.props.data[key]}
-					{/if}
-				</td>
-			</tr>
+			{#if data.props.data[key] != ''}
+				<tr>
+					<td>{key}</td>
+					<td>
+						{#if key == 'autorzy'}
+							<ul>
+								{#each data.props.data[key] as autor}
+									<li>{autor.imie} {autor.nazwisko}</li>
+								{/each}
+							</ul>
+						{:else if key == 'redaktorzy'}
+							<ul>
+								{#each data.props.data[key] as redaktor}
+									<li>{redaktor.imie_redaktor} {redaktor.nazwisko_redaktor}: {redaktor.rola}</li>
+								{/each}
+							</ul>
+						{:else if key == 'cytuje'}
+							<ul>
+								{#each data.props.data[key] as cyt}
+									<li>{cyt.tytul}</li>
+								{/each}
+							</ul>
+						{:else}
+							{data.props.data[key]}
+						{/if}
+					</td>
+				</tr>
+			{/if}
 		{/each}
 	</table>
 	<div />
